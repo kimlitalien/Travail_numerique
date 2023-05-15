f60e0ed3ff551a3e880fb6e0d2b03961042ddfb5
Amelie <amaud17@ulaval.ca>
2023-05-14 21:30:04 -0400

    Circuit d polaire

---
+++ examples/Circuit_d)_polaire.py
@@ -0,0 +1,67 @@
+import numpy as np 
+import env_examples
+from sympy import Symbol
+
+
+from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
+from src.laplace_equation_solver import LaplaceEquationSolver
+from src.biot_savart_equation_solver import BiotSavartEquationSolver
+
+
+if __name__ == "__main__":
+    WORLD_SHAPE = (101, 101)
+    BATTERY_VOLTAGE = 1.0
+    HIGH_WIRE_RESISTANCE = 1.0
+    LOW_WIRE_RESISTANCE = 0.01
+
+    polar_variables = Symbol("r"), Symbol("theta")
+    r, theta = polar_variables
+
+    #Vertical
+    r_vertical = 0 * r
+    theta_vertical = theta
+    eq_vertical = (r_vertical, theta_vertical)
+
+    #horizontal
+    r_horizontal = r
+    theta_horizontal = 0 * theta
+    eq_horizontal = (r_horizontal, theta_horizontal)
+
+    #pour l'angle (en diagonal)
+    r_diago = r
+    theta_diago = theta
+    eq_diago = (r_diago, theta_diago)
+
+    #constantes pour le circuit d)
+    theta1 = np.pi / 24
+    theta2 = np.pi / 3
+
+    wires = [
+        Wire((20, theta1), (80, theta1), eq_horizontal, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((80, theta1), (80, np.pi/7), eq_vertical, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((80, np.pi/7), (80, 2*np.pi/9), eq_vertical, polar_variables, HIGH_WIRE_RESISTANCE),
+        Wire((80, 2*np.pi/9), (80, theta2), eq_vertical, polar_variables, HIGH_WIRE_RESISTANCE),
+        Wire((80, theta2), (20, theta2), eq_horizontal, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((20, theta2), (20, 2*np.pi/9), eq_vertical, polar_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((20, 2*np.pi/9), (20, np.pi/7), eq_vertical, polar_variables, BATTERY_VOLTAGE),
+        Wire((20, np.pi/7), (20, theta1), eq_vertical, polar_variables, LOW_WIRE_RESISTANCE)
+    ]
+    ground = (20, np.pi/7)
+
+    circuit = Circuit(wires, ground)
+    world = World(circuit=circuit, coordinate_system=CoordinateSystem.POLAR, shape=WORLD_SHAPE)
+
+    world.show_circuit(
+        {0: (20, theta1),
+        1: (80, theta1), 
+        2: (80, np.pi/7),
+        3: (80, 2*np.pi/9),
+        4: (80, theta2),
+        5: (20, theta2),
+        6: (20, 2*np.pi/9),
+        7: (20, np.pi/7)}
+    )
+
+    world.compute()
+    world.show_all()
+
